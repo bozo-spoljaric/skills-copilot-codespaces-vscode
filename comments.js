ed3e817ea3cfd4b80001c81b3aba9a12c25c0f2f
bozo-spoljaric <40840193+bozo-spoljaric@users.noreply.github.com>
2023-08-05 15:39:37 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,62 +1,14 @@
 //create web server
 const express = require('express');
 const router = express.Router();
-//import the model
-const Comment = require('../models/Comment');
+const comments = require('../controllers/comments');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, validateComment, isCommentAuthor } = require('../middleware');
 
-//create route for comments
-//get all comments
-router.get('/', async (req, res) => {
-    try {
-        const comments = await Comment.find();
-        res.json(comments);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
+//create a comment
+router.post('/', isLoggedIn, validateComment, catchAsync(comments.createComment));
 
-//submit a comment
-router.post('/', async (req, res) => {
-    const comment = new Comment({
-        name: req.body.name,
-        comment: req.body.comment
-    });
-    try {
-        const savedComment = await comment.save();
-        res.json(savedComment);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
-
-//get specific comment
-router.get('/:commentId', async (req, res) => {
-    try {
-        const comment = await Comment.findById(req.params.commentId);
-        res.json(comment);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
-
-//delete specific comment
-router.delete('/:commentId', async (req, res) => {
-    try {
-        const removedComment = await Comment.remove({ _id: req.params.commentId });
-        res.json(removedComment);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
-
-//update a comment
-router.patch('/:commentId', async (req, res) => {
-    try {
-        const updatedComment = await Comment.updateOne({ _id: req.params.commentId }, { $set: { comment: req.body.comment } });
-        res.json(updatedComment);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
+//delete a comment
+router.delete('/:commentId', isLoggedIn, isCommentAuthor, catchAsync(comments.deleteComment));
 
 module.exports = router;
